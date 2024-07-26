4b8a76a0684d222773b0ded37bae08cf022a663c
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-26 17:11:19 +0900

    Typescriptの基本のデータ型を作成する

---
+++ test/src/test.ts
@@ -0,0 +1,6 @@
+export const test = () => {
+  const name: string = 'Mike'; //1. 文字列型 (string)
+  const age: number = 25; //2. 数値型 (number)
+  const isStudent: boolean = false; //3. ブール型 (boolean)
+  const hobbies: string[] = ['reading', 'coding', 'gaming']; //4. 配列型 (Array)
+};
