714df7ddb537ed0e2b01c14a10d843ad475d9580
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-29 15:25:17 +0900

    srcにtest3.tsを作成する

--- test/src/test3.ts
+++ test/src/test3.ts
@@ -12,14 +12,16 @@ export const test3 = () => {
       age: number,
       Email: string,
       isActive: boolean
-    );
-    this.userId = userId;
-    this.name = name;
-    this.age = age;
-    this.email = email;
-    this.isActive = isActive;
-  }
-  getProfile():string{
-    return `Name: ${this.name}, Age: ${this.age}`;
+    ) {
+      this.userId = userId; // Assign the value from the constructor parameter
+      this.name = name;
+      this.age = age;
+      this.Email = Email;
+      this.isActive = isActive;
+    }
+
+    getProfile(): string {
+      return `Name: ${this.name}, Age: ${this.age}`;
+    }
   }
 };
