9a0bc79bc24ffc3b9546dc1166c48410957eeed6
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-02 14:04:45 +0900

    JavaScriptで実装する

--- beginner/index.js
+++ beginner/index.js
@@ -1,4 +1,13 @@
 const books = [
   { title: 'TypeScript入門', author: '田中太郎', available: true },
   { title: 'JavaScript入門', author: '山田太郎', available: false },
+  { title: 'React入門', author: '鈴木太郎', available: true },
 ];
+
+const borrowedBooks = [];
+let newBookId = 1;
+
+function addNewBook(book) {
+  books.push(book);
+  return book;
+}
