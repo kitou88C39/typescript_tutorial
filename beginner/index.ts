97c202626cf31c827367f3302f8527c027f886b6
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-04 16:50:20 +0900

    index.tsを編集する

--- beginner/index.ts
+++ beginner/index.ts
@@ -27,9 +27,8 @@ const borrowedBooks: BorrowedBook[] = [];
 let newBookId = 1;
 
 //本が入荷した時に書籍リストに追加
-function addNewBook(book: Book) {
+function addNewBook(book: Book): void {
   books.push(book);
-  return book;
 }
 
 //借りたい本のタイトルを関数に渡すと貸してくれる
