9c08beb0ef06c2bd71efd94ac804777bd36c43f8
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-04 16:47:42 +0900

    function.tsを編集する

--- beginner/function.ts
+++ beginner/function.ts
@@ -4,9 +4,12 @@ export function getBookDetail(
   identifier: string | number,
   books: Book[]
 ): Book {
-    const book = typeof identifier === "number"? books.find(book => book.id === identifier):books.find(book => book.title === identifier):
-    if(!book){
-    throw new Error("本が見つかりませんでした")
-    }
-    
+  const book =
+    typeof identifier === 'number'
+      ? books.find((book) => book.id === identifier)
+      : books.find((book) => book.title === identifier);
+  if (!book) {
+    throw new Error('本が見つかりませんでした');
+  }
+  return book;
 }
