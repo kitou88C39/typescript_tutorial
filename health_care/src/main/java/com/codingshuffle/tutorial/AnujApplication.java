4745e28c072270bd879a2a1640c5c23dc051fd7e
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-04 20:53:49 +0900

    TypeScriptで健康管理アプリを作る

---
+++ health_care/src/main/java/com/codingshuffle/tutorial/AnujApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuffle.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnujApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnujApplication.class, args);
+	}
+
+}
